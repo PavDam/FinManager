a5afa24e9c7fd68ca58401f3bd572ba49e983575
PavDam <damian.pavlyk@gmail.com>
2024-06-05 16:52:26 +0300

    login and register forms created

--- src/main/java/com/mycompany/finmanagerpav/FinManagerPav.java
+++ src/main/java/com/mycompany/finmanagerpav/FinManagerPav.java
@@ -13,11 +13,18 @@ import com.mysql.cj.jdbc.MysqlDataSource;
 public class FinManagerPav {
 
     public static MysqlDataSource data = new MysqlDataSource(); 
+    public static int currentUserID;
+    public static String currentUsername;
     
     public static void main(String[] args) {
         data.setServerName("localhost");
         data.setPort(3306);
         data.setUser("root");
         data.setPassword("277353Damian");
+        data.setDatabaseName("finance-manager");
+        
+        LoginForm LoginF = new LoginForm();
+        // Відображення вікна
+        LoginF.setVisible(true);
     }
 }
