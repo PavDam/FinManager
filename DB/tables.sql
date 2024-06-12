563ad11ae8b66691fcbf974ddb2c4f03849debe8
PavDam <damian.pavlyk@gmail.com>
2024-06-12 12:01:42 +0300

    system categories added

--- DB/tables.sql
+++ DB/tables.sql
@@ -29,6 +29,19 @@ CREATE TABLE `category` (
   FOREIGN KEY (`UserID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+INSERT INTO `category` (`UserID`, `Name`, `Type`) VALUES
+(NULL, 'Продукти', 'Витрати'),
+(NULL, 'Розваги', 'Витрати'),
+(NULL, 'Зарплата', 'Отримання'),
+(NULL, 'Шопінг', 'Витрати'),
+(NULL, 'Премія', 'Отримання'),
+(NULL, 'Ресторани та бари', 'Витрати'),
+(NULL, 'Стипендія', 'Отримання'),
+(NULL, 'Товари для дому', 'Витрати'),
+(NULL, 'Інтернет-магазини', 'Витрати'),
+(NULL, 'Невідомо', 'Витрати'),
+(NULL, 'Невідомо', 'Отримання');
+
 CREATE TABLE `transaction` (
   `ID` INT NOT NULL AUTO_INCREMENT,
   `UserID` INT NOT NULL,
